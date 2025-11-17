a8a288d1cf236746aad0b79b22a0471d48225ad4
Jovite-Jeffrin <jovite.jeffrin17@gmail.com>
2025-11-17 17:49:13 +0530

    This is my second commit. Tried with different shots and outcomes

--- task_qrng.py
+++ task_qrng.py
@@ -1,3 +1,8 @@
+# from qrng.runner import run_qrng
+# result = run_qrng(num_outcomes=8, shots=100)
+# print("Random number:", result)
+
+# with the differnt shots and outvomes
 from qrng.runner import run_qrng
-result = run_qrng(num_outcomes=8, shots=100)
+result = run_qrng(num_outcomes=16, shots=500)
 print("Random number:", result)
