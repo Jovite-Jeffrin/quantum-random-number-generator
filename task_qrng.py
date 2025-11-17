83356893003e80fe497b433ccf0d086de16997bd
Jovite-Jeffrin <jovite.jeffrin17@gmail.com>
2025-11-17 17:49:51 +0530

    This is my third commit. Full code

--- task_qrng.py
+++ task_qrng.py
@@ -1,6 +1,6 @@
-# from qrng.runner import run_qrng
-# result = run_qrng(num_outcomes=8, shots=100)
-# print("Random number:", result)
+from qrng.runner import run_qrng
+result = run_qrng(num_outcomes=8, shots=100)
+print("Random number:", result)
 
 # with the differnt shots and outvomes
 from qrng.runner import run_qrng
